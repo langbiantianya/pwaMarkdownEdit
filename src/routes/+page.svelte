6f1192726b509ad94510a1f77364ba44a324dc27
lbty <langbiantianya@hotmail.com>
2026-04-07 12:03:42 +0800

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -107,61 +107,6 @@
             ],
         });
 
-        const customMenuTable = Cherry.createMenuHook("图表", {
-            iconName: "trendingUp",
-            subMenuConfig: [
-                {
-                    noIcon: true,
-                    name: "折线图",
-                    onclick: () => cherry?.insert(
-                        '\n| :line:{"title": "折线图"} | Header1 | Header2 | Header3 | Header4 |\n| ------ | ------ | ------ | ------ | ------ |\n| Sample1 | 11 | 11 | 4 | 33 |\n| Sample2 | 112 | 111 | 22 | 222 |\n| Sample3 | 333 | 142 | 311 | 11 |\n'
-                    ),
-                },
-                {
-                    noIcon: true,
-                    name: "柱状图",
-                    onclick: () => cherry?.insert(
-                        '\n| :bar:{"title": "柱状图"} | Header1 | Header2 | Header3 | Header4 |\n| ------ | ------ | ------ | ------ | ------ |\n| Sample1 | 11 | 11 | 4 | 33 |\n| Sample2 | 112 | 111 | 22 | 222 |\n| Sample3 | 333 | 142 | 311 | 11 |\n'
-                    ),
-                },
-                {
-                    noIcon: true,
-                    name: "雷达图",
-                    onclick: () => cherry?.insert(
-                        '\n| :radar:{"title": "雷达图"} | 技能1 | 技能2 | 技能3 | 技能4 | 技能5 |\n| ------ | ------ | ------ | ------ | ------ | ------ |\n| 用户A | 90 | 85 | 75 | 80 | 88 |\n| 用户B | 75 | 90 | 88 | 85 | 78 |\n| 用户C | 85 | 78 | 90 | 88 | 85 |\n'
-                    ),
-                },
-                {
-                    noIcon: true,
-                    name: "热力图",
-                    onclick: () => cherry?.insert(
-                        '\n| :heatmap:{"title": "热力图"} | 周一 | 周二 | 周三 | 周四 | 周五 |\n| ------ | ------ | ------ | ------ | ------ |\n| 上午 | 10 | 20 | 30 | 40 | 50 |\n| 下午 | 15 | 25 | 35 | 45 | 55 |\n| 晚上 | 5 | 15 | 25 | 35 | 45 |\n'
-                    ),
-                },
-                {
-                    noIcon: true,
-                    name: "饼图",
-                    onclick: () => cherry?.insert(
-                        '\n| :pie:{"title": "饼图"} | 数值 |\n| ------ | ------ |\n| 苹果 | 40 |\n| 香蕉 | 30 |\n| 橙子 | 20 |\n| 葡萄 | 10 |\n'
-                    ),
-                },
-                {
-                    noIcon: true,
-                    name: "散点图",
-                    onclick: () => cherry?.insert(
-                        '\n| :scatter:{"title": "散点图", "cherry:mapping": {"x": "X", "y": "Y", "size": "Size", "series": "Series"}} | X | Y | Size | Series |\n| ------ | ------ | ------ | ------ | ------ |\n| A1 | 10 | 20 | 5 | S1 |\n| A2 | 15 | 35 | 8 | S1 |\n| B1 | 30 | 12 | 3 | S2 |\n| B2 | 25 | 28 | 6 | S2 |\n| C1 | 50 | 40 | 9 | S3 |\n| C2 | 60 | 55 | 7 | S3 |\n'
-                    ),
-                },
-                {
-                    noIcon: true,
-                    name: "地图",
-                    onclick: () => cherry?.insert(
-                        '\n| :map:{"title": "地图", "mapDataSource": "https://geo.datav.aliyun.com/areas_v3/bound/100000_full.json"} | 数值 |\n| :-: | :-: |\n| 北京 | 100 |\n| 上海 | 200 |\n| 广东 | 300 |\n| 四川 | 150 |\n| 江苏 | 250 |\n| 浙江 | 180 |\n\n**说明：** 修改mapDataSource的URL来自定义地图数据源\n'
-                    ),
-                },
-            ],
-        });
-
         const defaultText = localStorage.getItem("lastMarkdown") || "";
         savedText = defaultText;
 
@@ -227,7 +172,6 @@
                     "italic",
                     {
                         strikethrough: [
-                            "strikethrough",
                             "underline",
                             "sub",
                             "sup",
@@ -246,27 +190,20 @@
                     "align",
                     "detail",
                     "|",
+                    "image",
+                    "link",
+                    "hr",
+                    "br",
+                    "code",
+                    "inlineCode",
                     "formula",
-                    {
-                        insert: [
-                            "image",
-                            "link",
-                            "hr",
-                            "br",
-                            "code",
-                            "inlineCode",
-                            "formula",
-                            "toc",
-                            "table",
-                        ],
-                    },
-                    "graph",
+                    "toc",
+                    "table",
                     "proTable",
                     "togglePreview",
                     "codeTheme",
                     "search",
                     "shortcutKey",
-                    "customMenuTable",
                     "customMenuCName",
                 ],
                 toolbarRight: ["changeLocale", "wordCount"],
@@ -287,7 +224,6 @@
                 customMenu: {
                     customMenuAName: customMenuA,
                     customMenuCName: customMenuC,
-                    customMenuTable,
                     customMenuFile,
                 },
                 shortcutKeySettings: { isReplace: false, shortcutKeyMap: {} },
